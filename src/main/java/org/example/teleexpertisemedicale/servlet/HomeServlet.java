c116dbdb8cd12dd8e4af7cff4215fa51f21fd814
salma hamdi <saalmahm51@gmail.com>
2025-10-10 23:06:58 +0100

    feat: Implémentation de la servlet Home

---
+++ src/main/java/org/example/teleexpertisemedicale/servlet/HomeServlet.java
@@ -0,0 +1,22 @@
+package org.example.teleexpertisemedicale.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * Servlet de la page d'accueil
+ */
+@WebServlet("")
+public class HomeServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
+    }
+}
