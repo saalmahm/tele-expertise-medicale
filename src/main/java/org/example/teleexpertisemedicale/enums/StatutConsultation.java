e935a29402c4c48f97d7dfd24615557a9729e689
salma hamdi <saalmahm51@gmail.com>
2025-10-10 23:02:14 +0100

    feat: Ajout de l'enum StatutConsultation

---
+++ src/main/java/org/example/teleexpertisemedicale/enums/StatutConsultation.java
@@ -0,0 +1,8 @@
+package org.example.teleexpertisemedicale.enums;
+
+
+public enum StatutConsultation {
+    EN_ATTENTE,    // Consultation programmée mais pas encore commencée
+    EN_COURS,      // Consultation en cours
+    TERMINEE       // Consultation terminée
+}
