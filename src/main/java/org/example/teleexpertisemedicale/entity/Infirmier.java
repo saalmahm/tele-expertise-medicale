6d0087948dd1dfa616013db1d6f7ac9f2e70d2df
salma hamdi <saalmahm51@gmail.com>
2025-10-18 23:24:25 +0100

    feat: Ajouter entité Infirmier héritant de User

--- src/main/java/org/example/teleexpertisemedicale/entity/Infirmier.java
+++ src/main/java/org/example/teleexpertisemedicale/entity/Infirmier.java
@@ -1,31 +1,8 @@
 package org.example.teleexpertisemedicale.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Infirmier extends User {
-
-    @OneToMany(mappedBy = "infirmier")
-    private List<Patient> patients = new ArrayList<>();
-
-    // Méthodes métiers
-    public void enregistrerPatient(Patient patient) {
-        patients.add(patient);
-    }
-
-    public List<Patient> voirListePatients() {
-        return patients;
-    }
-
-    // Getters / Setters
-    public List<Patient> getPatients() {
-        return patients;
-    }
-
-    public void setPatients(List<Patient> patients) {
-        this.patients = patients;
-    }
+    
 }
