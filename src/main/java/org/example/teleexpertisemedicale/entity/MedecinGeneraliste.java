6e75abbf33850801bf08f52d1d2d46a9b12f32df
salma hamdi <saalmahm51@gmail.com>
2025-10-18 23:24:31 +0100

    feat: Ajouter entité MedecinGeneraliste héritant de User

--- src/main/java/org/example/teleexpertisemedicale/entity/MedecinGeneraliste.java
+++ src/main/java/org/example/teleexpertisemedicale/entity/MedecinGeneraliste.java
@@ -1,31 +1,9 @@
 package org.example.teleexpertisemedicale.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
 import org.example.teleexpertisemedicale.enums.StatutConsultation;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class MedecinGeneraliste extends User {
 
-    @OneToMany(mappedBy = "medecinGeneraliste")
-    private List<Consultation> consultations = new ArrayList<>();
-
-    // Remplir une consultation
-    public void remplirConsultation(Consultation consultation, String raison, String observation) {
-        consultation.setRaison(raison);
-        consultation.setObservation(observation);
-        consultation.setStatut(StatutConsultation.TERMINEE);
-    }
-
-    // Getters / Setters
-
-    public List<Consultation> getConsultations() {
-        return consultations;
-    }
-
-    public void setConsultations(List<Consultation> consultations) {
-        this.consultations = consultations;
-    }
 }
